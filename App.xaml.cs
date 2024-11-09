435c7b9f9471252db2e694a25c85884bab44fa2c
Ingrid Ines Tchaptchet Nyassi <300140496@monboreal.ca>
2024-11-08 20:31:19 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppTchapi
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppTchapi
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
